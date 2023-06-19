973fa167b18cf776d227e9c2a6d4cd1d0558c93e
Mei <83911312+mskmei@users.noreply.github.com>
2023-06-19 16:44:28 +0800

    Update dyvit.py

--- models/dyvit.py
+++ models/dyvit.py
@@ -322,7 +322,7 @@ class VisionTransformerDiffPruning(nn.Module):
     def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=768, depth=12,
                  num_heads=12, mlp_ratio=4., qkv_bias=True, qk_scale=None, representation_size=None,
                  drop_rate=0., attn_drop_rate=0., drop_path_rate=0., hybrid_backbone=None, norm_layer=None, 
-                 pruning_loc=None, token_ratio=None, distill=False, viz=False):
+                 pruning_loc=None, token_ratio=None, distill=False, viz_mode=False):
         """
         Args:
             img_size (int, tuple): input image size
